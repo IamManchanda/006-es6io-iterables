d918f4ec1191fdd765034b167df5cbe08673e2ab
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-07 23:53:50 +0530

    For in with objects!

--- js/script.js
+++ js/script.js
@@ -34,3 +34,15 @@ for (const para of paraBunch) {
     console.log(this.textContent);
   });
 }
+
+const apple = {
+  color: 'red',
+  size: 'medium',
+  weight: 50,
+  sugar: 10,
+};
+
+for (const prop in apple) {
+  const value = apple[prop];
+  console.log(value, prop);
+}
