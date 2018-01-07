391fd25acfb5786a4cdf428d3364781be5db3cba
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-07 23:31:28 +0530

    For of loop!

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/**
+ * JS
+ */
+
+const cuts = ['Chuck', 'Brisket', 'Shank', 'Short Rib'];
+
+for (const [index, cut] of cuts.entries()) {
+  console.log(`${cut} is the ${index + 1} item.`);
+}
+
+function addUpNumbers() {
+  let total = 0;
+  for (num of arguments) {
+    total += num; 
+  }
+  console.log(total);
+  return total; 
+}
+
+addUpNumbers(10,23,52,34,12,13,123);
+
+const name = 'Harry Manchanda';
+for (const char of name) {
+  console.log(char);
+}
+
+const paraBunch = document.querySelectorAll('p');
+for (const para of paraBunch) {
+  para.addEventListener('click', function() {
+    console.log(this.textContent);
+  });
+}
